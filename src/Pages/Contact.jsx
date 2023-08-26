717e645516bbbf7bce0fc31d7d2008212d8b7d80
pitagora <codxd13@gmail.com>
2023-08-26 17:27:28 +0200

    made the about page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
     return(
         <div>
-            <h1>CONTACT PAGE</h1>
+            <div className="form-container"></div>
         </div>
     )
 }
